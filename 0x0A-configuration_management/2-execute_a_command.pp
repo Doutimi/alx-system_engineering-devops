499fae6de31809d5d7ec93dbcfad833f12cc0090
Doutimi <isenahdoutimiebi@gmail.com>
2024-02-29 15:05:57 +0100

    deleted files

--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,7 +0,0 @@
-# a manifest that kills a process named killmenow
-
-exec { 'kill_killmenow':
-  command => 'pkill killmenow',
-  path    => '/usr/bin/:/usr/local/bin/:/bin/',
-  onlyif  => 'pgrep killmenow',
-}
--- 0x0A-configuration_management/README.md
+++
@@ -1,7 +0,0 @@
-# 0x0A-configuration_management
-Learning about Puppet.
-
-##Tasks
-1. create a file
-2. Install a package
-3. Execute a command
